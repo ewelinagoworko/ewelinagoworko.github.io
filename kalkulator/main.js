87ee5f9926929462aa3ff314121b631b4c06723c
Ewelina Goworko <ewelina.goworko@gmail.com>
2017-07-03 14:37:58 +0200

    commit

---
+++ kalkulator/main.js
@@ -0,0 +1,25 @@
+function c(val){
+  
+    document.getElementById("screen").value = val;
+}
+
+function math(val){
+  
+    document.getElementById("screen").value += val;
+}
+
+
+function e() { 
+  
+    try { 
+      
+      c(eval(document.getElementById("screen").value)) 
+    } 
+    
+    catch(e) {
+      
+      c('Error') 
+    } 
+}
+
+
